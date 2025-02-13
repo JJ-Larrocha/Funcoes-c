44487992cf362f59b84c54f0582cf233a47f1a26
Pedro Leite <pedromatheushenry@outlook.com>
2025-02-13 19:34:27 -0400

    Corrigi o Começo
    Coloquei a variável "começo" pra funcionar.

--- CriaMenu.cpp
+++ CriaMenu.cpp
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LEN 20
-
-void Criar_Menu_While(char chave[2], char separador[2][LEN], char C, int n, FILE *fp);
-void Cria_Menu(char chave[2], char separador[2][LEN], int n, FILE *fp);
-void separacao(char separador[2][LEN], char crctr[2]);
+void Criar_Menu_While(char chave[2], char separador[], char C, int n, FILE *fp);
+void Cria_Menu(char chave[2], char separador[], int n, int comeco, FILE *fp);
+void separacao(char separador[], char caractere);
 int opcao(const char* txt);
 
 int main(){
     int n, w, comeco, opc1, opc2;
-    char chave[2], charSeparacao[2], C;
-    char separador[2][LEN];
+    char chave[3], separador[20], C;
     FILE *fp;
     
     fp = fopen("MENU.cpp", "w");
@@ -32,25 +29,25 @@ int main(){
         C = 'c';
     // tipo de selecionador
     puts("Tipo de selecionador:");
-    puts("(1)- ( )\t[2]- [ ]");
+	puts("(1)- ( )\t[2]- [ ]");
 	puts("{3}- { }\t|4|- | |");
 	scanf("%d", &opc1);
     
     switch(opc1){
         case (1):
-            chave[0]='('; chave[1]=')';
+            chave[1]='('; chave[2]=')';
         break;
 
         case (2):
-            chave[0]='['; chave[1]=']';
+            chave[1]='['; chave[2]=']';
         break;
 
         case (3):
-            chave[0]='{'; chave[1]='}';
+            chave[1]='{'; chave[2]='}';
         break;
 
         case (4):
-            chave[0]='|'; chave[1]='|';
+            chave[1]='|'; chave[2]='|';
         break;
 
         default:
@@ -59,93 +56,46 @@ int main(){
 
     if (opcao("Quer separador?\n[1]- Sim\t[0]- Nao\n")){    
         // tipo de separador
-        puts("Tipo do primeiro separador:");
+        puts("Tipo de separador:");
         puts("[1]- ===\t[2]- ---");
         puts("[3]- +++\t[4]- ###");
         puts("[5]- ***\t[6]- |||");
-        puts("[7]- Outro caractere");
         scanf("%d", &opc2);
 
-        switch(opc2){
+        switch(opc1){
             case (1):
-                charSeparacao[0] = '=';
+                separacao(separador, '=');
             break;
 
             case (2):
-                charSeparacao[0] = '-';
+                separacao(separador, '-');
             break;
 
             case (3):
-                charSeparacao[0] = '+';
+                separacao(separador, '+');
             break;
 
             case (4):
-                charSeparacao[0] = '#';
+                separacao(separador, '#');
             break;
 
             case (5):
-                charSeparacao[0] = '*';
+                separacao(separador, '*');
             break;
         
             case (6):
-                charSeparacao[0] = '|';
-            break;
-
-            case (7):
-                printf("Digite o caractere desejado: "); scanf("%c%*c", &charSeparacao[0]);
-            break;
-
-            default:
-                break;
-        }
-
-        puts("Tipo do Segundo separador:");
-        puts("[1]- ===\t[2]- ---");
-        puts("[3]- +++\t[4]- ###");
-        puts("[5]- ***\t[6]- |||");
-        puts("[7]- Outro caractere");
-        scanf("%d", &opc2);
-
-        switch(opc2){
-            case (1):
-                charSeparacao[1] = '=';
-            break;
-
-            case (2):
-                charSeparacao[1] = '-';
-            break;
-
-            case (3):
-                charSeparacao[1] = '+';
-            break;
-
-            case (4):
-                charSeparacao[1] = '#';
-            break;
-
-            case (5):
-                charSeparacao[1] = '*';
-            break;
-        
-            case (6):
-                charSeparacao[1] = '|';
-            break;
-
-            case (7):
-                printf("Digite o caractere desejado: "); scanf("%c", &charSeparacao[1]);
+                separacao(separador, '|');
             break;
 
             default:
                 break;
         }
     }
-
-    separacao(separador, charSeparacao);
     
     if(w)
         Criar_Menu_While(chave, separador, C, n, fp);
     else
-        Cria_Menu(chave, separador, n, fp);
+        Cria_Menu(chave, separador, n, comeco, fp);
 
     puts("Menu criado com sucesso!");
     fclose(fp);
@@ -153,7 +103,7 @@ return 0;
 }
 
 // criacao do menu
-void Criar_Menu_While(char chave[2], char separador[2][LEN], char C, int n, FILE *fp){
+void Criar_Menu_While(char chave[2], char separador[], char C, int n, FILE *fp){
     // Criacao do whille e passo.
     if (C == 'c')
         fprintf(fp, "bool pass = true; char opc;\n");
@@ -161,83 +111,115 @@ void Criar_Menu_While(char chave[2], char separador[2][LEN], char C, int n, FILE
         fprintf(fp, "bool pass = true; char opc;\n");
 
     fprintf(fp, "while (pass == true){\n");
-    fprintf(fp, "\tputs(\"%s\\n\");\n", separador[0]);// separador inicial
+    fprintf(fp, "\tputs(\"%s\\n\");\n", separador);// separador inicial
        
     // Inicio da criacao das opc. do menu
     for (int i=1; i<(n+1); i++){
         if (i % 2 != 0)
-        fprintf(fp, "\tputs(\"%c%d%c- ",chave[0], i, chave[1]);
+        fprintf(fp, "\tputs(\"%c%d%c- ",chave[1], i, chave[2]);
 
         else{
-             fprintf(fp, "\\t%c%d%c- \\n\");", chave[0], i, chave[1]);
+             fprintf(fp, "\\t%c%d%c- \\n\");", chave[1], i, chave[2]);
             fprintf(fp, "\n");
         }
     }// fim dacriacao
 
     if (n % 2 != 0) // se n = impar, fecha o print
-        fprintf(fp, "\\t%c0%c- sair\");", chave[0], chave[1]);
+        fprintf(fp, "\\t%c0%c- sair\");", chave[1], chave[2]);
     else
-        fprintf(fp, "\tputs(\"%c0%c- sair\");", chave[0], chave[1]);
+        fprintf(fp, "\tputs(\"%c0%c- sair\");", chave[1], chave[2]);
         
     fprintf(fp, "\n\tscanf(\"%%%c\", &opc);\n", C);
-    fprintf(fp, "\tputs(\"%s\");\n\n", separador[1]);// separador final
+    fprintf(fp, "\tputs(\"%s\");\n\n", separador);// separador final
 
     fprintf(fp, "\tswitch(opc){\n");// if
 
-    // Criacao do else if
+    // Criacao das opcoes do switch
     for (int i=1; i<(n+1); i++){
         fprintf(fp, "\tcase (%d):\n\tbreak;\n\n", i);
     }// Fim
         
-    //else
+    //defalt
     fprintf(fp, "\tcase (0):\n\t\tpass = false;\n\tbreak;\n");
     fprintf(fp, "\tdefault: \n\t\tputs(\"Opcao Inexistente!\");\n");
     fprintf(fp, "\t}\n");
     fprintf(fp, "}");
 }
 
-void Cria_Menu(char chave[2], char separador[2][LEN], int n, FILE *fp){
-    fprintf(fp, "puts(\"%s\");\n", separador[0]);// separador inicial
+void Cria_Menu(char chave[2], char separador[], int n, int comeco, FILE *fp){
+    fprintf(fp, "puts(\"%s\");\n", separador);// separador inicial
         
     // Inicio da criacao das opc. do menu
-    for (int i=1; i<(n); i++){
-        if (i % 2 != 0)
-            fprintf(fp, "puts(\"%c%d%c- ",chave[0], i, chave[1]);
+    
+    if(comeco){ // Se comecar em 1, crie aqui
+        fprintf(fp, "puts(\"%c1%c- ",chave[1], chave[2]);
+        
+        for (int i=2; i<(n); i++){
+            if (i % 2 != 0)
+                fprintf(fp, "puts(\"%c%d%c- ",chave[1], i, chave[2]);
 
-        else{
-            fprintf(fp, "\\t%c%d%c- \\n\");", chave[0], i, chave[1]);
-            fprintf(fp, "\n");
+            else{
+                fprintf(fp, "\\t%c%d%c- \\n\");", chave[1], i, chave[2]);
+                fprintf(fp, "\n");
+            }
         }
-    }// fim dacriacao
 
-    if (n % 2 != 0) // se n = impar, fecha o print
-        fprintf(fp, "\tputs(\"%c%d%c- \\n\");", chave[0], n, chave[1]);
-    else
-        fprintf(fp, "\\t%c%d%c- \\n\");", chave[0], n, chave[1]);
+        if (n % 2 != 0) // se n = impar, fecha o print
+            fprintf(fp, "puts(\"%c%d%c- \\n\");", chave[1], n, chave[2]);
+        else
+            fprintf(fp, "\\t%c%d%c- \\n\");", chave[1], n, chave[2]);
+    }// fim da criacao
+
+    else{
+        for (int i=0; i<(n); i++){
+            if (i % 2 != 0)
+                fprintf(fp, "puts(\"%c%d%c- ",chave[1], i, chave[2]);
+
+            else{
+                fprintf(fp, "\\t%c%d%c- \\n\");", chave[1], i, chave[2]);
+                fprintf(fp, "\n");
+            }
+        }
+
+        if (n % 2 != 0) // se n = impar, fecha o print
+            fprintf(fp, "puts(\"%c%d%c- \\n\");", chave[1], n, chave[2]);
+        else
+            fprintf(fp, "\\t%c%d%c- \\n\");", chave[1], n, chave[2]);
+    }// fim dacriacao
         
     fprintf(fp, "\nscanf(\"%%c%%*c\", &opc);\n");
-    fprintf(fp, "puts(\"%s\\n\");\n\n", separador[1]);// separador final
+    fprintf(fp, "puts(\"%s\\n\");\n\n", separador);// separador final
 
-    fprintf(fp, "switch(opc){\n");// if
+    fprintf(fp, "switch(opc){\n"); //Switch
 
-    // Criacao do else if
-    for (int i=1; i<(n+1); i++){
-        fprintf(fp, "\tcase (%d):\n\tbreak;\n\n", i);
-    }// Fim
+    if(comeco) { //Se comecar em 1:
+        // Criacao das opcoes do switch
+        for (int i=1; i<(n+1); i++){
+            fprintf(fp, "\tcase (%d):\n\tbreak;\n\n", i);
+        }// Fim
+            
+        //defalt
+        fprintf(fp, "\tdefault: \n\t\tputs(\"Opcao Inexistente!\");\n");
+        fprintf(fp, "}\n");
+    }
+    
+    else { //Se comecar em 0
+        // Criacao das opcoes do switch
+        for (int i=0; i<n; i++){
+            fprintf(fp, "\tcase (%d):\n\tbreak;\n\n", i);
+        }// Fim
         
-    //else
-    fprintf(fp, "\tcase (0):\n\t\tpass = false;\n\tbreak;\n");
-    fprintf(fp, "\tdefault: \n\t\tputs(\"Opcao Inexistente!\");\n");
-    fprintf(fp, "\t}\n");
-    fprintf(fp, "}");
+        //defalt
+        fprintf(fp, "\tcase (0):\n\t\tpass = false;\n\tbreak;\n");
+        fprintf(fp, "\tdefault: \n\t\tputs(\"Opcao Inexistente!\");\n");
+        fprintf(fp, "}\n");
+    }
 }
 
 /* Funcao para preencher o vetor com o separador escolhido */
-void separacao(char separador[2][LEN], char crctr[2]){
-    for(int i=0; i<2; i++){
-        for(int j=0; j<LEN-1; i++){
-            separador[i][j] = crctr[i];
-        }
+void separacao(char separador[], char caractere){
+    for(int i=0; i<21; i++){
+        separador[i] = caractere;
     }
 }
 
